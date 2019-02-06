47969dccc7c868b74cb581dc1e9122baccbd06e7
Felipe Rabelo <felipe.carbone@dito.com.br>
2019-02-06 14:33:52 -0200

    Finished universal handler

--- lxndr/routes.go
+++ lxndr/routes.go
@@ -1,59 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
 
+	"github.com/KHs000/lxndr/pkg/httphandler"
 	"github.com/KHs000/lxndr/pkg/identifier"
 	"github.com/KHs000/lxndr/pkg/mongo"
 	"github.com/KHs000/lxndr/pkg/rndtoken"
 )
 
 func exportRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		defaultRoute(w, r)
-	})
-
-	http.HandleFunc("/newUser", func(w http.ResponseWriter, r *http.Request) {
-		rb, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		type validBody struct {
-			Email string `json:"email"`
-		}
-		b := validBody{}
-		err = json.Unmarshal([]byte(rb), &b)
-		if err != nil {
-			log.Println("Invalid body.")
-			w.WriteHeader(400)
-			w.Write([]byte("400 - Bad Request"))
-			return
-		}
-
-		res, err := createUser(b.Email)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		log.Println(res)
-	})
+	httphandler.Handler("/", []string{}, defaultRoute)
+	httphandler.Handler("/newUser", []string{"email"}, createUser)
 }
 
-func defaultRoute(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "It works...") }
+func defaultRoute(b map[string]string) httphandler.Res {
+	resp := httphandler.Res{}
+	resp.S.Code = 200
+	resp.S.Message = "It works..."
+	return resp
+}
 
-func createUser(email string) (string, error) {
+func createUser(b map[string]string) httphandler.Res {
+	email := b["email"]
+	resp := httphandler.Res{}
+
 	isNew := identifier.ValidateNewUser(email)
 	if !isNew {
-		return "This email has already been registred", nil
+		resp.E.Code = 409
+		resp.E.Error = "This email has already been registred."
+		return resp
 	}
 
 	tkn, hash := rndtoken.SendToken(email)
@@ -63,16 +43,21 @@ func createUser(email string) (string, error) {
 	res := mongo.Insert(mongo.Conn, coll, newUser)
 
 	if id, ok := res.InsertedID.(primitive.ObjectID); ok {
-		return fmt.Sprintf("User Created. ID: %v.", primitive.ObjectID.String(id)), nil
+		resp.S.Code = 201
+		resp.S.Message = fmt.Sprintf(
+			"User Created. ID: %v.", primitive.ObjectID.String(id))
+		return resp
 	}
 
-	return "There might be an error, please retry your operation.", nil
+	resp.E.Code = 500
+	resp.E.Error = "There might be an error, please retry your operation."
+	return resp
 }
 
 func editUser(email string, data interface{}) {
 	isNew := identifier.ValidateNewUser(email)
 	if isNew {
-		log.Fatal("This email is not registred")
+		log.Fatal("This email is not registred.")
 	}
 
 	coll := mongo.Collection{Database: "lxndr", CollName: "user"}
---
+++ pkg/httphandler/vredun.go
@@ -0,0 +1,87 @@
+package httphandler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Error ..
+type Error struct {
+	Code  int
+	Error string
+}
+
+// Success ..
+type Success struct {
+	Code    int
+	Message string
+}
+
+// Res ..
+type Res struct {
+	E Error
+	S Success
+}
+
+// Body ..
+type Body struct {
+	Body map[string]string
+}
+
+func logAccess(r *http.Request) { log.Printf("Request at %q", r.URL.Path) }
+
+func validBody(rb []byte, ff []string) (*Body, *Error) {
+	b := make(map[string]string)
+	err := json.Unmarshal(rb, &b)
+	if err != nil {
+		log.Println("Could not read body.")
+		return nil, &Error{Code: 400, Error: "Could not read body."}
+	}
+
+	for _, f := range ff {
+		if _, ok := b[f]; !ok {
+			log.Printf("Missing parameter: %q in request body. /n", f)
+			return nil, &Error{
+				Code:  400,
+				Error: fmt.Sprintf("Missing parameter: %q in request body.", f)}
+		}
+	}
+
+	return &Body{Body: b}, nil
+}
+
+// Handler ..
+func Handler(path string, matchFields []string, f func(map[string]string) Res) {
+	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+		logAccess(r)
+
+		rb, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Oh-uh, something's not quite right."))
+			return
+		}
+
+		b, e := validBody(rb, matchFields)
+		if e != nil {
+			w.WriteHeader(e.Code)
+			w.Write([]byte(e.Error))
+			return
+		}
+
+		resp := f(b.Body)
+		if &resp.E != nil {
+			w.WriteHeader(resp.E.Code)
+			w.Write([]byte(resp.E.Error))
+			log.Println(resp.E.Error)
+			return
+		}
+
+		w.WriteHeader(resp.S.Code)
+		w.Write([]byte(resp.S.Message))
+		log.Println(resp.S.Message)
+	})
+}
